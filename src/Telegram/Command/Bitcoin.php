be3a08d8586a5a8ff7b4d763ab8fb8f2afc23bd1
Alberto Rodriguez <arodu.dev@gmail.com>
2023-01-22 12:49:40 -0400

    fix:

--- src/Telegram/Command/Bitcoin.php
+++ src/Telegram/Command/Bitcoin.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Telegram\Command;
 
 use Cake\Core\Configure;
+use Cake\Utility\Hash;
 use TeBo\Telegram\Command\BaseCommand;
 use TeBo\Telegram\Response\Text;
 use TeBo\Telegram\Update;
@@ -23,34 +24,31 @@ class Bitcoin extends BaseCommand
     {
         $price = $this->getBitcoinPrice();
         $update->getChat()->send(new Text('The current price of bitcoin is $' . $price));
-
         //$update->getChat()->send(new Text('Custom start command!'));
     }
 
-
-    // method to find the price of bitcoin by the coinmarketcap v2 api, returns the price in USD, user CakeHttp from Cakephp
     public function getBitcoinPrice()
     {
-        $url = 'https://pro-api.coinmarketcap.com/v2/ticker/1/?convert=USD';
+        $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
         $headers = [
-            'Accepts: application/json',
-            'X-CMC_PRO_API_KEY: ' . Configure::read('CoinMarketCapApiKey'),
+            'Accepts' => 'application/json',
+            'X-CMC_PRO_API_KEY' => Configure::read('CoinMarketCapApiKey'),
         ];
         $qs = [
-            'start' => '1',
-            'limit' => '5000',
+            //'start' => '1',
+            //'limit' => '5000',
             'convert' => 'USD'
         ];
 
         $request = [
             'headers' => $headers,
-            'qs' => $qs
         ];
 
         $client = new \Cake\Http\Client();
-        $response = $client->get($url, $request);
+        $response = $client->get($url, $qs, $request);
         $data = $response->getJson();
-        $price = $data['data']['quotes']['USD']['price'];
+
+        $price = Hash::get($data, 'data.0.quote.USD.price');
         return $price;
     }
 }
